92a73ed30ed275ffc2bcf48202f70894fe66ba76
Debasish Saha Pranta <debasish.saha@brainstation23.com>
2020-05-07 11:46:50 +0600

    redux setup error

---
+++ src/store/reducers/index.ts
@@ -0,0 +1,8 @@
+import counterReducer from './counterReducer';
+import isLoggedReducer from './isLoggedReducer'
+import { combineReducers } from 'redux';
+
+const allReducers = combineReducers({
+    counter: counterReducer,
+    isLogged: isLoggedReducer,
+});
