46281835d9868d60a3d0a8a2711fa4ff27eb49f2
Debasish Saha Pranta <debasish.saha@brainstation23.com>
2020-05-13 16:10:52 +0600

    polygon works

--- src/Screens/City.tsx
+++ src/Screens/City.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { fetchData } from '../store/actions'
-import { Viewer, Entity, Camera, PolygonGraphics, Billboard, BillboardCollection } from "resium";
-import { Cartesian3, createWorldTerrain, Color, Transforms, PerspectiveOffCenterFrustum } from "cesium";
-import icon from '../assets/icon.png'
+import { Viewer, Entity, Camera, PolygonGraphics } from "resium";
+import { Cartesian3, createWorldTerrain, Color, } from "cesium";
 const terrainProvider = createWorldTerrain();
 
 
@@ -36,19 +35,69 @@ const City: React.FC<CityProps> = (props) => {
 
     //const counterState = useSelector((state: stateType) => state.counterReducer);
     //const weatherState = useSelector((state: stateType) => state.apiReducer);
-    const position = Cartesian3.fromDegrees(props.coordinate.longitude, props.coordinate.latitude, 300);
 
+    // const position = Cartesian3.fromDegrees(props.coordinate.longitude, props.coordinate.latitude, 300);
+    const position = Cartesian3.fromDegrees(136.86033081664658, 35.001719978413306, 1000);
     const position1 = Cartesian3.fromDegrees(props.coordinate.longitude + .001, props.coordinate.latitude + .001, 300);
-    const direction = Cartesian3.fromDegrees(90, 180, 180);
-    const center = Cartesian3.fromDegrees(props.coordinate.longitude, props.coordinate.latitude);
+
+    const direction = Cartesian3.fromDegrees(0, 0, 180);
+
     const pointGraphics = { pixelSize: 10 };
+
     const positionArray = Cartesian3.fromDegreesArray([
-        props.coordinate.longitude + .0001, props.coordinate.latitude + .0001,
-        props.coordinate.longitude + .0002, props.coordinate.latitude + .0002,
-        props.coordinate.longitude + .0003, props.coordinate.latitude + .0003,
-        props.coordinate.longitude + .0004, props.coordinate.latitude + .0004,
+        props.coordinate.longitude, props.coordinate.latitude,
+        props.coordinate.longitude + .02, props.coordinate.latitude + .01,
+        props.coordinate.longitude + .06, props.coordinate.latitude + .01,
+    ]);
+
+    const positionArray1 = Cartesian3.fromDegreesArray([
+
+
+        136.86033081664658,
+        35.001719978413306,
+
+        136.86065941083928,
+        35.001714940570146,
+
+        136.86065328782666,
+        35.00144456045727,
+
+        136.86032469471377,
+        35.00144959825013,
+
+
+        136.86033081664658,
+        35.001719978413306,
+
 
     ]);
+
+    const positionArrayTest = [
+        [
+            136.86033081664658,
+            35.001719978413306
+        ],
+        [
+            136.86065941083928,
+            35.001714940570146
+        ],
+        [
+            136.86065328782666,
+            35.00144456045727
+        ],
+        [
+            136.86032469471377,
+            35.00144959825013
+        ],
+        [
+            136.86033081664658,
+            35.001719978413306
+        ]
+    ]
+
+
+
+
 
 
     return (
@@ -58,30 +107,37 @@ const City: React.FC<CityProps> = (props) => {
                 terrainProvider={terrainProvider}
             >
 
-                <Camera position={position} direction={direction} />
-                <Entity position={position} point={pointGraphics} name={props.cityName} description="one." />
-                <Entity position={position1} point={pointGraphics} name={props.cityName} description="two." />
-                {/* 
-                <Entity>
+                {/* <Camera position={position} direction={direction} /> */}
+
+                {/* <Entity position={position} point={pointGraphics} name={props.cityName} description="one." />
+                <Entity position={position1} point={pointGraphics} name={props.cityName} description="two." /> */}
+
+
+
+
+
+                {positionArrayTest.map((item, index) => {
+                    return <Entity
+                        key={index}
+                        position={Cartesian3.fromDegrees(item[0], item[1], 50)}
+                        point={pointGraphics}
+                    >
+                    </Entity>
+
+                })}
+
+
+                <Entity onClick={() => console.log('hello')}>
                     <PolygonGraphics
-                        hierarchy={positionArray}
-                        height={1}
+                        hierarchy={positionArray1}
+                        height={50}
                         material={Color.RED.withAlpha(0.5)}
                         outline={true}
-                        outlineColor={Color.BLACK}
+                        outlineColor={Color.GREEN}
                         show={true}
-                        outlineWidth={2}
+                        outlineWidth={100}
                     />
-                </Entity> */}
-                <BillboardCollection modelMatrix={Transforms.eastNorthUpToFixedFrame(center)}>
-                    <Billboard
-                        color={Color.RED}
-                        position={new Cartesian3(0.0, 1000000.0, 0.0)}
-                        image={icon}
-                        scale={0.1}
-                    />
-                </BillboardCollection>
-
+                </Entity>
 
             </Viewer>
         </div>
